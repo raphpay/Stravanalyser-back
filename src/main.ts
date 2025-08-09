3b832702f99a8a82b41a73f3ec248f234632c0dc
raphpay <raphpay.dev@gmail.com>
2025-08-09 16:44:05 +0400

    Simple test to front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONT_END_URL ?? 'http://localhost:5173',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
